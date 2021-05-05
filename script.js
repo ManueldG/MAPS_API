2e8e3361333d99c7c6f3399932e9f2bca199af80
Manuel della Gala <74384683+ManueldG@users.noreply.github.com>
2021-05-05 12:32:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 41.9, lng: 11.644 },
+    center: { lat: 41.9770498146804, lng: 12.041022056337022 },
     zoom: 9,
   });
 }
+
+
